e9b742501e9fdb176687ab21d7bce1b10f8e8744
fanzuo1230 <1634563298@qq.com>
2020-12-04 20:01:46 +0800

    AI

--- ai/car-detect/index.js
+++ ai/car-detect/index.js
@@ -1,7 +1,25 @@
-// console.log('AI来了，帅！')
-// node 引入模 块的关键字
-const  ApiImageClassifyClient= require('baidu-aip-sdk').imageClassify;//图像识别
-//内置模块  filesSystem
-const fs= require('fs');
+// console.log('AI来了，酷！');
 
-// 1. 读入文件 I/O  从硬盘读入内存中,
+//de 引入模块的关键字  require
+// 图像识别
+const ApiImageClassifyClient=require('baidu-aip-sdk').imageClassify;
+//内置模块 fileSystem
+const fs = require('fs');
+
+// 1. 读入文件 I/O  从硬盘读入内存中，
+const APP_ID = "23099204";
+const API_KEY ="R3vzBnimh59KwIUkzA6XqN6M"
+const SECRET_KEY = "saQGM9YLVG0hnmVeF84R0u9Up7n0RQ3B"
+
+const client = new ApiImageClassifyClient(APP_ID,API_KEY,SECRET_KEY);
+// const fs = require('fs');
+const image =fs.readFileSync("timg.jpg").toString("base64");
+
+client
+    .carDetect(image)
+    .then(function(result){
+        console.log(result);
+    })
+    .catch(function(err){
+        console.log(err);
+    })
